7408355b72f32e143b18578cbb114ce006076581
Jim Martens <github@2martens.de>
2025-02-05 21:46:20 +0100

    style: Apply formatting

--- src/components/BlogOverview.tsx
+++ src/components/BlogOverview.tsx
@@ -2,7 +2,17 @@ import type { CollectionEntry } from "astro:content";
 import { buildPostUrl } from "../utils/postUrl";
 import { buildImageSrc } from "../utils/imageUrl";
 
-export default function BlogOverview({title, description, posts, basehref}: {title: string, description: string, posts: CollectionEntry<'posts'>[]|CollectionEntry<'speeches'>[], basehref: string}): any {
+export default function BlogOverview({
+  title,
+  description,
+  posts,
+  basehref,
+}: {
+  title: string;
+  description: string;
+  posts: CollectionEntry<"posts">[] | CollectionEntry<"speeches">[];
+  basehref: string;
+}): any {
   return (
     <div className="bg-white py-16 sm:py-24">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
@@ -10,21 +20,22 @@ export default function BlogOverview({title, description, posts, basehref}: {tit
           <h2 className="text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">
             {title}
           </h2>
-          <p className="mt-2 text-lg/8 text-gray-600">
-            {description}
-          </p>
+          <p className="mt-2 text-lg/8 text-gray-600">{description}</p>
         </div>
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {posts.map((post) => (
             <article
               key={post.id}
-              className="flex max-w-xl flex-col items-start justify-between"
+              className="flex flex-col max-w-xl items-start justify-between"
             >
               <div className="flex items-center gap-x-4 text-xs">
-                <time dateTime={post.data.publishedAt.toString()} className="text-gray-500">
+                <time
+                  dateTime={post.data.publishedAt.toString()}
+                  className="text-gray-500"
+                >
                   {new Date(post.data.publishedAt).toLocaleDateString("de-DE")}
                 </time>
-                {'category' in post.data && post.data.category && (
+                {"category" in post.data && post.data.category && (
                   <a
                     href={post.data.category.href}
                     className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
@@ -33,25 +44,31 @@ export default function BlogOverview({title, description, posts, basehref}: {tit
                   </a>
                 )}
               </div>
-              <div className="group relative">
+              <div className="relative group">
                 <h3 className="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-gray-600">
-                {'category' in post.data && post.data.category && (
-                  <a href={`${basehref}/${post.data.category.slug}/${buildPostUrl(post)}`} data-astro-prefetch>
-                    <span className="absolute inset-0" />
-                    {post.data.title}
-                  </a>
-                ) || (
-                  <a href={`${basehref}/${buildPostUrl(post)}`} data-astro-prefetch>
-                    <span className="absolute inset-0" />
-                    {post.data.title}
-                  </a>
-                )}
+                  {("category" in post.data && post.data.category && (
+                    <a
+                      href={`${basehref}/${post.data.category.slug}/${buildPostUrl(post)}`}
+                      data-astro-prefetch
+                    >
+                      <span className="absolute inset-0" />
+                      {post.data.title}
+                    </a>
+                  )) || (
+                    <a
+                      href={`${basehref}/${buildPostUrl(post)}`}
+                      data-astro-prefetch
+                    >
+                      <span className="absolute inset-0" />
+                      {post.data.title}
+                    </a>
+                  )}
                 </h3>
-                <p className="mt-5 line-clamp-3 text-sm/6 text-gray-600">
+                <p className="h-auto mt-5 line-clamp-3 text-sm/6 text-gray-600">
                   {post.data.description}
                 </p>
               </div>
-              <div className="relative mt-8 flex items-center gap-x-4">
+              <div className="flex relative mt-8 items-center gap-x-4">
                 <img
                   alt={post.data.author.image.alt}
                   src={buildImageSrc(post.data.author.image.url)}
