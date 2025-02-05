c25e4f29d62d0bceb52366fd2d005772b5a590c5
Jim Martens <github@2martens.de>
2025-02-05 20:18:12 +0100

    change: Don't show description

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -2,11 +2,11 @@ import { toDateAndTimeString } from "../utils/dateAndTime";
 import { renderRichText } from "../utils/lexicalConverter";
 import { type CollectionEntry } from "astro:content";
 
-export default function Post({ post }: { post: CollectionEntry<"posts"> }) {
+export default function Post({ post }: { post: CollectionEntry<"posts">|CollectionEntry<"speeches"> }) {
   return (
     <div className="bg-white px-6 py-16 lg:px-8">
       <article className="mx-auto max-w-3xl text-base/7 text-gray-700">
-        {post.data.category && (
+        {'category' in post.data && post.data.category && (
           <p className="text-base/7 font-semibold text-indigo-600">
             {post.data.category.title}
           </p>
@@ -14,9 +14,8 @@ export default function Post({ post }: { post: CollectionEntry<"posts"> }) {
         <h1 className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">
           {post.data.title}
         </h1>
-        <p className="mt-6 text-xl/8">{post.data.description}</p>
         {renderRichText(post.data.content, "mt-10 max-w-2xl")}
-        <p className="mt-6 text-sm">Last Modified: {toDateAndTimeString(post.data.updatedAt)}</p>
+        <p className="mt-6 text-sm">Letztes Update: {toDateAndTimeString(post.data.updatedAt)}</p>
       </article>
     </div>
   );
