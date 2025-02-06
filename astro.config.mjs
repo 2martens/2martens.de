eda805b6b87b098bea6e73225a6a0c6dd8011d0d
Jim Martens <github@2martens.de>
2025-02-06 08:24:01 +0100

    feat: Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,15 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   base: "/",
+  site: "https://2martens.de",
   trailingSlash: "never",
   prefetch: true,
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), sitemap()],
   vite: {
     ssr: {
       noExternal: ['@payloadcms/richtext-lexical']
