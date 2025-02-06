580d54e1335a7ed7f65fffc695bf4c7d86bc937c
Jim Martens <github@2martens.de>
2025-02-06 09:01:51 +0100

    fix: Apply correct link back to homepage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,7 +24,7 @@ export default function Header({
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
       >
         <div className="flex lg:flex-1">
-          <a href={base} className="-m-1.5 p-1.5">
+          <a href={`${base}/`} className="-m-1.5 p-1.5">
             <span className="text-sm/6 font-semibold text-gray-900">
               {logoTitle}
             </span>
@@ -65,7 +65,7 @@ export default function Header({
         <div className="fixed inset-0 z-10" />
         <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href={base} className="-m-1.5 p-1.5">
+            <a href={`${base}/`} className="-m-1.5 p-1.5">
               <span className="text-sm/6 font-semibold text-gray-900">{logoTitle}</span>
             </a>
             <button
