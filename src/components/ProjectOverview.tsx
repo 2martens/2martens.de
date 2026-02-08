175515532d9360b40f213f29acbe0a9c4470093f
Jim Martens <github@2martens.de>
2026-02-08 15:15:13 +0100

    feat: Added ability for multiple projects
    change: Updated Font Awesome package
    Add sieve script project

--- src/components/ProjectOverview.tsx
+++ src/components/ProjectOverview.tsx
@@ -1,53 +1,96 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { projects } from "@data/projects";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {type Project} from "@data/projects";
 
-export default function ProjectOverview(props: any) {
-  return (
-    <div className="overflow-hidden py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-          <div className="lg:pt-4 lg:pr-8">
-            <div className="lg:max-w-lg">
-              <h2 className="text-base/7 font-semibold">
-                {projects[0].category}
-              </h2>
-              <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-primary dark:text-primary-dark sm:text-5xl">
-                {projects[0].name}
-              </p>
-              <p className="mt-6 text-lg/8">{projects[0].description}</p>
-              <dl className="mt-10 max-w-xl space-y-8 text-base/7 lg:max-w-none">
-                {projects[0].features.map((feature) => (
-                  <div key={feature.name} className="relative pl-9">
-                    <dt className="inline font-semibold text-primary dark:text-primary-dark">
-                      <FontAwesomeIcon
-                        icon={feature.icon}
-                        aria-hidden="true"
-                        className="absolute top-1 left-1 size-5"
-                      />
-                      {feature.name}
-                    </dt>{" "}
-                    <dd className="inline">{feature.description}</dd>
-                  </div>
-                ))}
-              </dl>
+export default function ProjectOverview({
+                                            project,
+                                            isLeft = true,
+                                            image
+                                        }: {
+    project: Project,
+    isLeft: boolean,
+    image: any,
+}): any {
+    return (
+        <div className="overflow-hidden py-24 sm:py-32">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                <div
+                    className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+                    {(isLeft && (
+                        <div className="lg:pt-4 lg:pr-8">
+                            <div className="lg:max-w-lg">
+                                <h2 className="text-base/7 font-semibold">
+                                    {project.category}
+                                </h2>
+                                <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-primary dark:text-primary-dark sm:text-5xl">
+                                    {project.name}
+                                </p>
+                                <p className="mt-6 text-lg/8">{project.description}</p>
+                                <dl className="mt-10 max-w-xl space-y-8 text-base/7 lg:max-w-none">
+                                    {project.features.map((feature) => (
+                                        <div key={feature.name} className="relative pl-9">
+                                            <dt className="inline font-semibold text-primary dark:text-primary-dark">
+                                                <FontAwesomeIcon
+                                                    icon={feature.icon}
+                                                    aria-hidden="true"
+                                                    className="absolute top-1 left-1 size-5"
+                                                />
+                                                {feature.name}
+                                            </dt>
+                                            {" "}
+                                            <dd className="inline">{feature.description}</dd>
+                                        </div>
+                                    ))}
+                                </dl>
+                            </div>
+                        </div>
+                    ))}
+                    {image}
+                    {(!isLeft && (
+                        <div className="lg:pt-4 lg:pl-8">
+                            <div className="lg:max-w-lg">
+                                <h2 className="text-base/7 font-semibold">
+                                    {project.category}
+                                </h2>
+                                <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-primary dark:text-primary-dark sm:text-5xl">
+                                    {project.name}
+                                </p>
+                                <p className="mt-6 text-lg/8">{project.description}</p>
+                                <dl className="mt-10 max-w-xl space-y-8 text-base/7 lg:max-w-none">
+                                    {project.features.map((feature) => (
+                                        <div key={feature.name} className="relative pl-9">
+                                            <dt className="inline font-semibold text-primary dark:text-primary-dark">
+                                                <FontAwesomeIcon
+                                                    icon={feature.icon}
+                                                    aria-hidden="true"
+                                                    className="absolute top-1 left-1 size-5"
+                                                />
+                                                {feature.name}
+                                            </dt>
+                                            {" "}
+                                            <dd className="inline">{feature.description}</dd>
+                                        </div>
+                                    ))}
+                                </dl>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
-          </div>
-          {props.image}
+            {project.cta.show && (
+                <div className="mx-auto max-w-7xl text-center mt-10">
+                    <h3 className="text-2xl font-semibold tracking-tight text-body dark:text-body-dark sm:text-3xl">
+                        <span className="text-body dark:text-body-dark">{project.cta.call}</span>
+                    </h3>
+                    <div className="mt-10 flex items-center justify-center gap-x-6">
+                        <a
+                            href={project.cta.href}
+                            className="rounded-md bg-primary dark:bg-primary px-3.5 py-2.5 text-sm font-semibold text-white dark:text-body-dark shadow-xs hover:bg-primary-200 dark:hover:bg-primary-dark focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-dark"
+                        >
+                            {project.cta.action}
+                        </a>
+                    </div>
+                </div>
+            )}
         </div>
-      </div>
-      <div className="mx-auto max-w-7xl text-center mt-10">
-        <h3 className="text-2xl font-semibold tracking-tight text-body dark:text-body-dark sm:text-3xl">
-          <span className="text-body dark:text-body-dark">{projects[0].cta.call}</span>
-        </h3>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
-          <a
-            href={projects[0].cta.href}
-            className="rounded-md bg-primary dark:bg-primary px-3.5 py-2.5 text-sm font-semibold text-white dark:text-body-dark shadow-xs hover:bg-primary-200 dark:hover:bg-primary-dark focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-dark"
-          >
-            {projects[0].cta.action}
-          </a>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -1,6 +1,7 @@
 import { byPrefixAndName, type IconDefinition } from "@awesome.me/kit-217da5ee1c/icons";
 import type { ImageMetadata } from "astro";
 import timetable from "../assets/cologne-overview.jpeg"
+import appOverview from "../assets/sieve-script-editor.png"
 
 export interface Project {
     category: string;
@@ -14,6 +15,7 @@ export interface Project {
     image: ImageMetadata;
     imageAltText: string;
     cta: {
+        show: boolean;
         call: string;
         action: string;
         href: string;
@@ -21,6 +23,36 @@ export interface Project {
 }
 
 export const projects: Project[] = [
+    {
+        category: 'App Development',
+        name: 'Sieve Script Editor',
+        description: 'I created a macOS app to manage and edit sieve scripts',
+        features: [
+            {
+                name: 'Add and delete sieve scripts',
+                description: 'Add and delete sieve scripts to/from your account',
+                icon: byPrefixAndName.fas["file-plus-minus"],
+            },
+            {
+                name: 'Edit sieve scripts',
+                description: 'Edit your sieve scripts right from your device, including syntax highlighting, syntax validation and the ability to indent/outdent.',
+                icon: byPrefixAndName.fas["file-code"],
+            },
+            {
+                name: 'Mark sieve scripts as active',
+                description: 'Only one sieve script can be active but you can choose which one it is. Other files can be included from the one.',
+                icon: byPrefixAndName.fas["file-circle-check"],
+            },
+        ],
+        image: appOverview,
+        imageAltText: 'A large, modern train station with an arched glass and steel roof, filled with multiple red regional trains and a white high-speed train on separate tracks. The platforms have passengers walking, and blue signs with white text provide directions.',
+        cta: {
+            show: true,
+            call: "Start testing now",
+            action: "Enroll in testing",
+            href: "https://testflight.apple.com/join/qzNFutsv"
+        }
+    },
     {
         category: 'Modding',
         name: '24h Timetable Cologne-Aachen',
@@ -45,9 +77,10 @@ export const projects: Project[] = [
         image: timetable,
         imageAltText: 'A large, modern train station with an arched glass and steel roof, filled with multiple red regional trains and a white high-speed train on separate tracks. The platforms have passengers walking, and blue signs with white text provide directions.',
         cta: {
+            show: true,
             call: "Start playing now",
             action: "Download",
             href: "https://www.trainsimcommunity.com/mods/c3-train-sim-world/c111-timetables/i4387-cologne-aachen-timetable-2023-version-15"
         }
-    }
+    },
 ]
